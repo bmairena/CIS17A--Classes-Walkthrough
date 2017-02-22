9e1c2fbbafebfe3717b0d3d8700817eb902c20d7
ncc <ncc>
2017-02-22 09:48:43 -0800

    Teacher class finished

--- Classes/Classes/main.cpp
+++ Classes/Classes/main.cpp
@@ -1,9 +1,15 @@
-#include "Student.h"
 #include <iostream>
+#include "Teacher.h"
+#include "Student.h"
 using namespace std;
 
 int main()
 {
-	
+	Teacher james = Teacher("james");
+	Teacher bob = Teacher("bob");
+	cout << james.GetName() << endl;
+	cout << bob.GetName() << endl;
+
+	getchar();
 	return 0;
 }
